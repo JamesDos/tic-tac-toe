b0009981408a981f7701644cbb814ca69ad1dac5
James Tu <jamestvvo@gmail.com>
2024-01-10 00:12:09 -0500

    Implemented single player and computer mode logic
    - TODO: Display winner of game, allow user to chose player, display current player

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const createCell = () => {
-  let cellValue = "E";
+  let cellValue = "";
 
-  const isEmpty = () => cellValue === "E";
+  const isEmpty = () => cellValue === "";
 
   const getCellValue = () => cellValue;
 
@@ -33,8 +33,6 @@ const GameBoard = (() =>  {
         hasWon = true;
       }
     })
-    console.log('local check');
-    printBoard();
     return hasWon;
   }
 
@@ -72,6 +70,31 @@ const GameBoard = (() =>  {
     return (values1.every(value => value == mark) || values2.every(value => value == mark));
   }
 
+  const determineTie = () => {
+    let tied = true;
+    for(let i = 0; i < rows; i++) {
+      for(let j = 0; j < columns; j++) {
+        if(gameArr[i][j].getCellValue() === "") {
+          tied = false;
+        }
+      }
+    }
+    console.log(tied ? `tied` : `not tied`);
+    return tied;
+  }
+
+  const resetBoard = () => {
+    for(let i = 0; i < columns; i++) {
+      for(let j = 0; j < columns; j++) {
+        gameArr[i][j].setCellValue("");
+      }
+    }
+  }
+
+  const setTile = (i, j, mark) => gameArr[i][j].setCellValue(mark);
+
+  const getTileAt = (i, j) => gameArr[i][j];
+  
   const getGameArr = () => gameArr;
 
   const printBoard = () => {
@@ -79,30 +102,83 @@ const GameBoard = (() =>  {
     console.table(res);
   }
 
-  return {getGameArr, determineGameOver, printBoard};
+  return {determineGameOver, determineTie, resetBoard, setTile, getTileAt, getGameArr, printBoard, };
 
 })();
 
-const createPlayer = (name) => {
-  return {name};
+const createPlayer = (name, bot) => {
+
+  const isBot = () => bot === true;
+
+  const getName = () => name;
+
+  const selectTile = () => {
+    if(isBot()) {
+      let iVal = Math.floor(Math.random() * 3);
+      let jVal =  Math.floor(Math.random() * 3);
+      return {iVal, jVal};
+    }
+  }
+
+  return {getName, isBot, selectTile};
 }
 
 const GameController = (() => {
-  const player1 = createPlayer("Player1");
-  const player2 = createPlayer("Player2");
+  let player1 = createPlayer("Player1", false);
+  let player2 = createPlayer("Player2", false);
   let currPlayer = player1;
   const board = GameBoard;
+  const gameStates = ["Playing", "End"];
+  let gameState = "Playing";
 
-  const playRound = (x, y) => {
-    let gameArr = board.getGameArr();
-    if((gameArr[x][y]).isEmpty) {
-      gameArr[x][y].setCellValue("X");
+  const getGameState = () => gameState;
+
+  const setGameState = (state) => gameState = state;
+
+  const playRound = (i, j) => {
+    if(getGameState() === "Playing") {
+      if(board.getTileAt(i, j).isEmpty()) {
+        if(currPlayer === player1) {
+          board.setTile(i, j, "X");
+        } else {
+          board.setTile(i, j, "O");
+        }
+        determineGameState();
+      } else {
+        console.log('Cell is already taken')
+      }
+    }
+    board.printBoard();
+  }
+
+  const playRoundBot = (i, j) => {
+    if(getGameState() === "Playing") {
+      if (board.getTileAt(i, j).isEmpty()) {
+        board.setTile(i, j, "X");
+        determineGameState();
+        if(getGameState() === "Playing") {
+          let botChoice = player2.selectTile();
+          while(!(board.getTileAt(botChoice.iVal, botChoice.jVal).isEmpty())) {
+            botChoice = player2.selectTile();
+          }
+          board.setTile(botChoice.iVal, botChoice.jVal, "O");
+          determineGameState();
+        }
+      }
     } else {
       console.log('Cell is already taken')
     }
-    board.printBoard();
+  }
+
+  const determineGameState = () => {
     if(board.determineGameOver()) {
-      console.log('game has ended')
+      setGameState(gameStates[1]);
+      console.log(`game has ended: ${getCurrPlayer().getName()} has won`);
+    } else if (board.determineTie()) {
+      setGameState(gameStates[1]);
+      console.log('game is a draw')
+    } else {
+      switchPlayer()
     }
   }
 
@@ -114,13 +190,120 @@ const GameController = (() => {
     }
   }
 
+  const resetGame = () => {
+    board.resetBoard();
+    currPlayer = player1;
+    setGameState(gameStates[0]);
+  }
+
   const getCurrPlayer = () => currPlayer;
 
-  return {playRound, getCurrPlayer, board}
+  const setPlayer2 = (name, isBot) => {
+    if(isBot) {
+      player2 = createPlayer("Computer", true);
+    } else {
+      player2 = createPlayer(name, false);
+    }
+  }
+
+  return {playRound, playRoundBot, getCurrPlayer, resetGame, setPlayer2}
 })();
 
-GameController.playRound(2, 0);
-GameController.playRound(1, 1);
-GameController.playRound(0, 2);
+
+const DisplayGame = (() => {
+  const resetButton = document.querySelector('#reset-btn');
+  const gameboardHtml = document.querySelector('.gameboard');
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+  const playerSelection = document.querySelector('#player-select');
+
+  tiles.forEach(tile => tile.addEventListener('click', () => selectCell(tile)));
+
+  const renderBoard = () => {
+    clearBoard();
+    let gameboard = GameBoard.getGameArr();
+    gameboard.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        generateCell(cell.getCellValue(), i, j);
+      })
+    })
+  }
+
+  const generateCell = (mark, i, j) => {
+    let div = document.createElement('div');
+    div.classList.add('tile');
+    div.setAttribute('data-tile', mark);
+    div.setAttribute('data-ipos', String(i));
+    div.setAttribute('data-jpos', String(j));
+    div.textContent = mark;
+    div.addEventListener('click', () => selectCell(div));
+    gameboardHtml.appendChild(div);
+  }
+
+  const clearBoard = () => {
+    gameboardHtml.innerHTML = '';
+  }
+
+  const selectCell = tile => {
+    console.log(tile);
+    let iPos = parseInt(tile.dataset.ipos);
+    let jPos = parseInt(tile.dataset.jpos);
+    console.log(iPos, jPos);
+    if(playerSelection.value === "player") {
+      GameController.playRound(iPos, jPos);
+    }
+    if(playerSelection.value === "computer") {
+      GameController.playRoundBot(iPos, jPos);
+    }
+    renderBoard();
+  }
+
+  const switchGameMode = () => {
+    const player2 = playerSelection.value;
+    if (player2 === "player") {
+      GameController.setPlayer2("Player2", false);
+    } if (player2 === "computer") {
+      GameController.setPlayer2("Player2", true);
+    }
+    resetBoard();
+  }
+
+  playerSelection.addEventListener('change', switchGameMode);
+
+  const resetBoard = () => {
+    GameController.resetGame();
+    renderBoard();
+  }
+
+  resetButton.addEventListener('click', resetBoard);
+
+})();
+
+
+// tying 
+// GameController.playRound(1, 1);
+// GameController.playRound(0, 0);
+// GameController.playRound(1, 0);
+// GameController.playRound(1, 2);
+// GameController.playRound(2, 2);
+// GameController.playRound(2, 1);
+// GameController.playRound(2, 0);
+// GameController.playRound(0, 2);
+// GameController.playRound(0, 1);
+
+// X wins
+// GameController.playRound(0, 0);
+// GameController.playRound(1, 0);
+// GameController.playRound(0, 1);
+// GameController.playRound(1, 1);
+// GameController.playRound(0, 2);
+
+// O wins
+  // GameController.playRound(0, 2);
+  // GameController.playRound(0, 0);
+  // GameController.playRound(0, 1);
+  // GameController.playRound(1, 0);
+  // GameController.playRound(1, 1);
+  // GameController.playRound(2, 0);
+
 
 
